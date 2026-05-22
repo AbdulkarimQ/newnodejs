21a58f5f9d7f95f9a8b0ddbfe27ec0440b99079a
Abedulkareem03 <abedulkarimq@mail.tau.ac.il>
2026-05-22 17:57:47 +0300

    as

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.sendFile("./views/home.html", {root : __dirname})
+  res.send("hello")
 });
 
 app.listen(port, () => {
